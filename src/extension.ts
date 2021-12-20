48723b9b85c754e3f1e25d452b214278a7cea347
shutaozhenzhen <3462232313@qq.com>
2021-12-20 23:50:49 +0800

    remove command

--- src/extension.ts
+++ src/extension.ts
@@ -17,13 +17,6 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showErrorMessage("Your vscode.git extension are disabled. You can go to setting and search `git:enabled` to enable it.");
 		return;
 	}
-	let disposable = vscode.commands.registerCommand('note-git.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from note-git!');
-	});
-
-	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
